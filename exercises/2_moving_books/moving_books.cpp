4ba2b5814c218eaa939c30b0d3a58ded9dffdec5
Alberto Montes <alm59321@gmail.com>
2016-10-12 10:30:44 +0200

    Solve Moving books exercise

--- exercises/2_moving_books/moving_books.cpp
+++ exercises/2_moving_books/moving_books.cpp
@@ -1,7 +1,11 @@
 #include <vector>
+#include <set>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+
 
 void moving_books() {
     long n; cin >> n;
@@ -15,6 +19,33 @@ void moving_books() {
         cin >> w[i];
     }
 
+    // Check if the heviest box can not be bringed by nobody
+    int max_s = *max_element(s.begin(), s.end());
+    int max_w = *max_element(w.begin(), w.end());
+    if (max_w > max_s) {
+        cout << "impossible\n";
+        return;
+    }
+
+
+    multiset<int, greater<int> > ws;
+    sort(s.begin(), s.end(), greater<int>());
+
+    for (int i = 0; i < m; i++) ws.insert(w[i]);
+    int r = 0;
+    while (!ws.empty()) {
+        r++;
+        for (int i = 0; i < n; i++) {
+            auto j = ws.lower_bound(s[i]);
+            if (j != ws.end()) {
+                ws.erase(j);
+            } else {
+                break;
+            }
+        }
+    }
+
+    cout << 3*r-1 << endl;
 
 }
 

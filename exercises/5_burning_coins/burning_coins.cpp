1b2ef1eaf3f27e373264ded86116baff684c89d2
Alberto Montes <alm59321@gmail.com>
2016-10-26 16:50:22 +0200

    Solve burning coins exercise

--- exercises/5_burning_coins/burning_coins.cpp
+++ exercises/5_burning_coins/burning_coins.cpp
@@ -3,20 +3,52 @@
 
 using namespace std;
 
+
+int play(vector<int>& coins_values, int left_idx, int right_idx, bool turn, vector<vector<int> >& mem) {
+    /*  turn = true  -> Your turn
+        turn = false -> Friend's turn */
+
+    // Final case if no coins left
+    if (left_idx == right_idx) {
+        return 0;
+    }
+
+    if (mem[left_idx][right_idx-1] > -1) {
+        return mem[left_idx][right_idx-1];
+    }
+
+    // First take the left coins then the right one
+    int left_result, right_result, result;
+    if (turn) {
+        left_result = coins_values[left_idx];
+        left_result += play(coins_values, left_idx+1, right_idx, false, mem);
+        right_result = coins_values[right_idx-1];
+        right_result += play(coins_values, left_idx, right_idx-1, false, mem);
+        result = (left_result > right_result) ? left_result : right_result;
+        mem[left_idx][right_idx-1] = result;
+        return result;
+    } else {
+        left_result = play(coins_values, left_idx+1, right_idx, true, mem);
+        right_result = play(coins_values, left_idx, right_idx-1, true, mem);
+        result = (left_result < right_result) ? left_result: right_result;
+        mem[left_idx][right_idx-1] = result;
+        return result;
+    }
+}
+
+
 void burning_coins() {
     int n; cin >> n;
 
-    int odd = 0, even = 0;
+    vector<int> coins_values(n);
     for (int i = 0; i < n; i++) {
-        int v; cin >> v;
-        if (i % 2 == 0) {
-            even += v;
-        } else {
-            odd += v;
-        }
+        cin >> coins_values[i];
     }
 
-    cout << min(even, odd) << endl;
+    // Add some memory storing the result for every pair of left and right indexes possible (speedup)
+    vector<vector<int> > mem(n, vector<int>(n, -1));
+    cout << play(coins_values, 0, n, true, mem) << endl;
+
 
 }
 

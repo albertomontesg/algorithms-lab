a85fe5b5396d2485f4e3a1fcba96a22f9206088e
Alberto Montes <alm59321@gmail.com>
2016-10-31 18:12:41 +0100

    Add PoW 6 statement

---
+++ problems_of_the_week/6_a_new_hope/test.sh
@@ -0,0 +1,12 @@
+NAME=a_new_hope
+
+echo "Running tests"
+for input in testsets/*.in ; do
+    echo $input
+    time ./$NAME.o < $input > $input.tmp ;
+done
+for output in testsets/*.out ; do
+    echo $output
+    diff $output ${output%.out}.in.tmp ;
+done
+rm testsets/*.in.tmp

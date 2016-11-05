fce70a9d13579e785c194817701464e68c20d6ff
Alberto Montes <alm59321@gmail.com>
2016-11-05 10:11:54 +0100

    Start Inball exercise

---
+++ exercises/7_inball/inball.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <CGAL/basic.h>
+#include <CGAL/QP_models.h>
+#include <CGAL/QP_functions.h>
+#include <CGAL/Gmpz.h>
+
+// choose exact integral type
+typedef CGAL::Gmpz ET;
+
+// program and solution types
+typedef CGAL::Quadratic_program<long> Program;
+typedef CGAL::Quadratic_program_solution<ET> Solution;
+
+using namespace std;
+
+
+void inball(int n){
+    int d; cin >> d;
+
+    Program lp (CGAL::SMALLER, false, 0, false, 0);
+
+    lp.set_c(d, -1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < d; j++) {
+            long a; cin >> a;
+            lp.set_a(j, i, a);
+        }
+        lp.set_a(d, i, 1);
+        long b; cin >> b;
+        lp.set_b(i, b);
+    }
+    lp.set_l(d, 0);
+
+    Solution s = CGAL::solve_linear_program(lp, ET());
+    assert (s.solves_linear_program(lp));
+
+
+    if (s.is_infeasible()) {
+        cout << "none" << endl;
+    } else if (s.is_unbounded()) {
+        cout << "inf" << endl;
+    } else {
+        long result = -CGAL::to_double(s.objective_value());
+        cout << result << endl;
+    }
+
+}
+
+
+int main() {
+    int n;
+    while (true) {
+        cin >> n;
+        if (n == 0) { break; }
+        inball(n);
+    }
+}
--- exercises/7_inball/test.sh
+++ exercises/7_inball/test.sh
@@ -3,10 +3,10 @@ NAME=inball
 echo "Running tests"
 for input in testsets/*.in ; do
     echo $input
-    time ./$NAME.o < $input > $input.tmp ;
+    time ./$NAME < $input > $input.tmp ;
 done
 for output in testsets/*.out ; do
     echo $output
     diff $output ${output%.out}.in.tmp ;
 done
-rm testsets/*.in.tmp
+#rm testsets/*.in.tmp

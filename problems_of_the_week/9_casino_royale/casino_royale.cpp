dfb4563e837515f0a833ff2fcf26a78f34c30471
Alberto Montes <alm59321@gmail.com>
2016-11-24 08:24:06 +0100

    Tried to solve PoW 9

---
+++ problems_of_the_week/9_casino_royale/casino_royale.cpp
@@ -0,0 +1,126 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+// Namespaces
+using namespace std;
+using namespace boost;
+
+
+// BGL Graph definitions
+// =====================
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef adjacency_list_traits<vecS, vecS, directedS> Traits;
+typedef adjacency_list<vecS, vecS, directedS, no_property,
+    property<edge_capacity_t, long,
+        property<edge_residual_capacity_t, long,
+            property<edge_reverse_t, Traits::edge_descriptor,
+                property <edge_weight_t, long> > > > > Graph;
+// Interior Property Maps
+typedef property_map<Graph, edge_capacity_t>::type      EdgeCapacityMap;
+typedef property_map<Graph, edge_weight_t >::type       EdgeWeightMap;
+typedef property_map<Graph, edge_residual_capacity_t>::type ResidualCapacityMap;
+typedef property_map<Graph, edge_reverse_t>::type       ReverseEdgeMap;
+typedef graph_traits<Graph>::vertex_descriptor          Vertex;
+typedef graph_traits<Graph>::edge_descriptor            Edge;
+typedef graph_traits<Graph>::out_edge_iterator          OutEdgeIt; // Iterator
+
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map, weight map and reverse edge map
+// ===============================================================
+class EdgeAdder {
+    Graph &G;
+    EdgeCapacityMap &capacitymap;
+    EdgeWeightMap &weightmap;
+    ReverseEdgeMap  &revedgemap;
+
+public:
+    EdgeAdder(Graph & G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap)
+        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+
+    void addEdge(int u, int v, long c, long w) {
+        Edge e, reverseE;
+        tie(e, tuples::ignore) = add_edge(u, v, G);
+        tie(reverseE, tuples::ignore) = add_edge(v, u, G);
+        capacitymap[e] = c;
+        weightmap[e] = w;
+        capacitymap[reverseE] = 0;
+        weightmap[reverseE] = -w;
+        revedgemap[e] = reverseE;
+        revedgemap[reverseE] = e;
+    }
+};
+
+
+
+void casino_royale() {
+    int n, m, l;
+    cin >> n >> m >> l;
+
+    const int MAX_PRIORITY = 1<<7;
+
+    // Create Graph and Maps
+    Graph G(n+2);
+    EdgeCapacityMap capacitymap = get(edge_capacity, G);
+    EdgeWeightMap weightmap = get(edge_weight, G);
+    ReverseEdgeMap revedgemap = get(edge_reverse, G);
+    ResidualCapacityMap rescapacitymap = get(edge_residual_capacity, G);
+    EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+    Vertex src = n, sink = n+1;
+
+    for (int i = 0; i < n-1; i++) { eaG.addEdge(i, i+1, l, 0); }
+
+    for (int i = 0; i < m; i++) {
+        int x, y, q;
+        cin >> x >> y >> q;
+        eaG.addEdge(src, x, 1, MAX_PRIORITY - q);
+        eaG.addEdge(y, sink, 1, 0);
+    }
+
+    // Compute flow and cost
+    successive_shortest_path_nonnegative_weights(G, src, sink);
+    int cost = find_flow_cost(G);
+    int flow = 0, f;
+
+    // for (int i = 0; i < n; i++) {
+    //     Edge e; bool success;
+    //     tie(e, success) = edge(src, i, G);
+    //     if (success) {
+    //         f = capacitymap[e] - rescapacitymap[e];
+    //         flow += f;
+    //         cost -= f * MAX_PRIORITY;
+    //     }
+    //     tie(e, success) = edge(i, sink, G);
+    //     if (success) {
+    //         f = capacitymap[e] - rescapacitymap[e];
+    //         cost -= f * MAX_PRIORITY;
+    //     }
+    // }
+
+    OutEdgeIt e, eend;
+    for(tie(e, eend) = out_edges(vertex(src,G), G); e != eend; ++e) {
+        int f = capacitymap[*e] - rescapacitymap[*e];
+        flow += f;
+        cost -= f * MAX_PRIORITY;
+    }
+    cost = -cost;
+
+    cout << flow << " " << cost << endl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int T; cin >> T;
+    for (int t=0; t < T; t++){
+        casino_royale();
+    }
+}

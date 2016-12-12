15bd0ca68f60218803b848294cdb0ef382e70997
Alberto Montes <alm59321@gmail.com>
2016-12-12 12:57:59 +0100

    Add examples code

---
+++ examples/bgl/flows.cpp
@@ -0,0 +1,110 @@
+// ALGOLAB BGL Tutorial 2
+// Flow example demonstrating
+// - interior graph properties for flow algorithms
+// - custom edge adder
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 flows.cpp -o flows ./flows
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 flows.cpp -o flows; ./flows
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/edmonds_karp_max_flow.hpp>
+// Namespaces
+using namespace std;
+using namespace boost;
+
+
+// BGL Graph definitions
+// =====================
+// Graph Type with nested interior edge properties for Flow Algorithms
+typedef	adjacency_list_traits<vecS, vecS, directedS> Traits;
+typedef adjacency_list<vecS, vecS, directedS, no_property,
+	property<edge_capacity_t, long,
+		property<edge_residual_capacity_t, long,
+			property<edge_reverse_t, Traits::edge_descriptor> > > >	Graph;
+// Interior Property Maps
+typedef	property_map<Graph, edge_capacity_t>::type		EdgeCapacityMap;
+typedef	property_map<Graph, edge_residual_capacity_t>::type	ResidualCapacityMap;
+typedef	property_map<Graph, edge_reverse_t>::type		ReverseEdgeMap;
+typedef	graph_traits<Graph>::vertex_descriptor			Vertex;
+typedef	graph_traits<Graph>::edge_descriptor			Edge;
+
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map and reverse edge map
+// ===================================================
+class EdgeAdder {
+	Graph &G;
+	EdgeCapacityMap	&capacitymap;
+	ReverseEdgeMap	&revedgemap;
+
+public:
+	// to initialize the Object
+	EdgeAdder(Graph & G, EdgeCapacityMap &capacitymap, ReverseEdgeMap &revedgemap):
+		G(G), capacitymap(capacitymap), revedgemap(revedgemap){}
+
+	// to use the Function (add an edge)
+	void addEdge(int from, int to, long capacity) {
+		Edge e, reverseE;
+		bool success;
+		tie(e, success) = add_edge(from, to, G);
+		tie(reverseE, success) = add_edge(to, from, G);
+		capacitymap[e] = capacity;
+		capacitymap[reverseE] = 0;
+		revedgemap[e] = reverseE;
+		revedgemap[reverseE] = e;
+	}
+};
+
+
+// Functions
+// =========
+// Function for an individual testcase
+void testcases() {
+	// Create Graph and Maps
+	Graph G(4);
+	EdgeCapacityMap capacitymap = get(edge_capacity, G);
+	ReverseEdgeMap revedgemap = get(edge_reverse, G);
+	ResidualCapacityMap rescapacitymap = get(edge_residual_capacity, G);
+	EdgeAdder eaG(G, capacitymap, revedgemap);
+
+	// Add edges
+	eaG.addEdge(0, 1, 1); // from, to, capacity
+	eaG.addEdge(0, 3, 1);
+	eaG.addEdge(2, 1, 1);
+	eaG.addEdge(2, 3, 1);
+
+	// Add source and sink
+	// Careful: The names 'source' and 'target' are already used for BGL's 
+	// functions to get the two endpoints of an edge, use 'src' and 'sink'.
+	Vertex src = add_vertex(G);
+	Vertex sink = add_vertex(G);
+	eaG.addEdge(src, 0, 2);
+	eaG.addEdge(src, 2, 1);
+	eaG.addEdge(1, sink, 2);
+	eaG.addEdge(3, sink, 1);
+
+	// Calculate flow
+	// If not called otherwise, the flow algorithm uses the interior properties
+	// - edge_capacity, edge_reverse (read access),
+	// - edge_residual_capacity (read and write access).
+	long flow1 = push_relabel_max_flow(G, src, sink);
+	long flow2 = edmonds_karp_max_flow(G, src, sink);
+	cout << flow1 << " == " << flow2 << endl;
+}
+
+// Main function to loop over the testcases
+int main() {
+	ios_base::sync_with_stdio(false);
+	int T;	T = 1;
+	for (; T > 0; --T)	testcases();
+	return 0;
+}

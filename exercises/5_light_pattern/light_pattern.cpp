9bedcd5cd75faaa89892475670d4af57cffc21bf
Alberto Montes <alm59321@gmail.com>
2016-12-26 10:38:52 +0100

    Tried with Light Pattern exercise

--- exercises/5_light_pattern/light_pattern.cpp
+++ exercises/5_light_pattern/light_pattern.cpp
@@ -1,22 +1,33 @@
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
 
-
-
-
 void light_pattern() {
-    int n, k, x;
+    int n, k, x, p, b, s, q;
     cin >> n >> k >> x;
+    p = n / k;
 
-    vector<bool> bulbs_state(n);
-    for (int i = 0; i < n; i++) {
-        int b; cin >> b;
-        bulbs_state[i] = (b == 1) ? true: false;
+    vector<unsigned short int> bulbs_state(p), pattern(p);
+    for (int i = 0; i < p; i++) {
+        s = 0; q = 0;   // s to store the bulb state, q to store the pattern
+        for (int j = 0; j < k; j++) {
+            cin >> b;
+            if (b == 1) s |= 1 << j;
+            if (j % 2 == 0) q |= (1 - (x & 1)) << j;
+            else q |= (x & 1) << j;
+        }
+        bulbs_state[i] = s;
+        pattern[i] = q;
+        cout << q;
     }
+    cout << endl;
 
+    /*
+    Go through the blocks from left to right. Is there a recursive relationship between the number of operations needed to get blocks 1..i to have the right patterns and the number of operations needed to get blocks 1..i-1 to have the right patterns. If so, you could do DP. If not, why not, and how could it be fixed? This should point you into the right direction.
+    */
 
 
 }

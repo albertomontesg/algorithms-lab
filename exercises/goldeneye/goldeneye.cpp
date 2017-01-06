e31ec0ed1f5e0c4eb87ed3addb281d2a4e8856d5
Alberto Montes <alm59321@gmail.com>
2017-01-06 12:55:50 +0100

    Solve Golden Eye exercise

--- exercises/goldeneye/goldeneye.cpp
+++ exercises/goldeneye/goldeneye.cpp
@@ -1,154 +1,97 @@
-#include <vector>
-#include <map>
-#include <stack>
-
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Triangulation_face_base_2.h>
+#include <vector>
+#include <boost/pending/disjoint_sets.hpp>
 
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/connected_components.hpp>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel     K;
+typedef K::Point_2                                              P;
+typedef CGAL::Triangulation_vertex_base_with_info_2<int,K>      Vb;
+typedef CGAL::Triangulation_face_base_2<K>                      Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb,Fb>             Tds;
+typedef CGAL::Delaunay_triangulation_2<K,Tds>                   Delaunay;
+typedef Delaunay::Vertex_handle                                 VertexH;
+typedef Delaunay::Finite_edges_iterator                         EI;
+typedef std::pair<P,int>                                        IPoint;
+typedef boost::disjoint_sets_with_storage<>                     Uf;
 
 using namespace std;
-using namespace boost;
-
-// BGL typedefs
-typedef adjacency_list<vecS, vecS, undirectedS, no_property, no_property>   Graph;
-typedef graph_traits<Graph>::edge_descriptor                                Edge;
-typedef graph_traits<Graph>::vertex_descriptor                              Vertex;
-// CGAL typedefs
-typedef CGAL::Exact_predicates_inexact_constructions_kernel         K;
-typedef CGAL::Triangulation_vertex_base_with_info_2<unsigned, K>    Vb;
-typedef CGAL::Triangulation_data_structure_2<Vb>                    Tds;
-typedef CGAL::Delaunay_triangulation_2<K, Tds>                      Triangulation;
-typedef Triangulation::Finite_faces_iterator                        FaceIt;
-typedef Triangulation::Finite_edges_iterator                        EdgeIt;
-typedef Triangulation::Face                                         Face;
-typedef Triangulation::Face_handle                                  FaceH;
-typedef Triangulation::Vertex_handle                                VertexH;
-typedef K::Point_2                                                  Point;
-typedef K::Segment_2                                                Segment;
-typedef K::Triangle_2                                               Triangle;
 
 
-Graph build_graph(Triangulation& t, int w) {
-    // Build a graph given the w power consumption checking the distances between vertex adding
-    // edges if the w value allows it
-    int n = t.number_of_vertices();
-    Graph G(n);
+struct Edge {
+    Edge(int u_, int v_, K::FT sql_) : u(u_), v(v_), sql(sql_) {}
+    int u, v;       // endpoints
+    K::FT sql;      // squared length
+};
 
-    Edge e; bool success;
-    EdgeIt ei;
-    FaceIt ft;
-    map<Edge, bool> edge_seen;
-    VertexH vh_a, vh_b;
-    for ( ei = t.finite_edges_begin(); ei != t.finite_edges_end(); ei++ ) {
-        Face f = *(ei->first);
-        int i = ei->second;
-        VertexH vs, vt;
-        vs = f.vertex(f.cw(i));
-        vt = f.vertex(f.ccw(i));
-        K::FT d = CGAL::squared_distance(vs->point(), vt->point());
-        if ( d <= w ) {
-            tie(e, success) = add_edge(vs->info(), vt->info(), G);
-        }
-
-    }
-
-    return G;
-
-}
-
-bool check_mission(Triangulation& tri, vector<pair<Point, long> >& j, vector<int>& component,
-        Point s, Point t, int w) {
-    int a = tri.nearest_vertex(s)->info();
-    int b = tri.nearest_vertex(t)->info();
-
-    int d1 = CGAL::squared_distance(s, j[a].first);
-    int d2 = CGAL::squared_distance(t, j[b].first);
-
-    if (d1 > w/4 || d2 > w/4) return false;
-    else if (component[a] == component[b]) return true;
-    else return false;
-}
-
-int binary_search(Triangulation& tri, vector<pair<Point, int> >& j,
-        vector<Point>& s, vector<Point>& t) {
-
-
-    return 0;
-}
-
-int evaluate(Triangulation& tri, vector<pair<Point, long> >& j, long p, vector<Point>& s, vector<Point>& t) {
-    Graph G = build_graph(tri, p);
-    int m = s.size(), n = j.size();
-
-    // Compute the connected components
-    vector<int> component(n);
-    connected_components(G, &component[0]);
-
-    // Check all the missions
-    int missions_completed = 0;
-    for (int i = 0; i < m; i++) {
-        bool cm = check_mission(tri, j, component, s[i], t[i], p);
-        if ( cm ) {
-            missions_completed |= (1<<i);
-        }
-    }
-
-    return missions_completed;
-}
+inline bool operator<(const Edge& e, const Edge& f) { return e.sql < f.sql; }
 
 void goldeneye() {
-    long n, m, p;
+    int n, m; double p;
     cin >> n >> m >> p;
 
-    // Define variables
-    vector<pair<Point, long> > j(n);
-    vector<Point> s(m), t(m);
+    // Read jammers and build Delaunay
+    vector<IPoint> jammers(n);
     long x, y;
-    // Read input data
     for (int i = 0; i < n; i++) {
         cin >> x >> y;
-        j[i] = make_pair(Point(x, y), i);
+        jammers[i] = IPoint(P(x, y), i);
     }
+    Delaunay t;
+    t.insert(jammers.begin(), jammers.end());
+
+    // Extract edges and sort by length
+    vector<Edge> edges;
+    edges.reserve(3*n);
+    for (EI e = t.finite_edges_begin(); e != t.finite_edges_end(); e++) {
+        edges.push_back(Edge(e->first->vertex((e->second+1)%3)->info(),
+                             e->first->vertex((e->second+2)%3)->info(),
+                             t.segment(e).squared_length()));
+    }
+    sort(edges.begin(), edges.end());
+
+    // Compute components with power consumption p
+    Uf ufp(n);
+    typedef vector<Edge>::const_iterator ECI;
+    for (ECI e = edges.begin(); e != edges.end() && e->sql <= p; e++)
+        ufp.union_set(e->u, e->v);
+
+    // Handle missions
+    K::FT a = 0, b = 0;
+    Uf ufa(n), ufb(n);
+    ECI ai = edges.begin(), bi = edges.begin();
+    int x0, y0, x1, y1;
     for (int i = 0; i < m; i++) {
-        cin >> x >> y;
-        s[i] = Point(x, y);
-        cin >> x >> y;
-        t[i] = Point(x, y);
+        cin >> x0 >> y0 >> x1 >> y1;
+        P p0(x0, y0), p1(x1, y1);
+        VertexH v0 = t.nearest_vertex(p0), v1 = t.nearest_vertex(p1);
+        K::FT d = 4 * max(CGAL::squared_distance(p0, v0->point()),
+                          CGAL::squared_distance(p1, v1->point()));
+
+        if (d <= p && ufp.find_set(v0->info()) == ufp.find_set(v1->info())) {
+            // mission possible with power p -> also with b
+            cout << "y";
+            if (d > b) b = d;
+            for (; bi != edges.end() &&
+                    ufb.find_set(v0->info()) != ufb.find_set(v1->info()); bi++)
+                ufb.union_set(bi->u, bi->v);
+        } else cout << "n";
+        // ensure it is possible at power a
+        if (d > a) a = d;
+        for (; ai != edges.end() &&
+                ufa.find_set(v0->info()) != ufa.find_set(v1->info()); ai++)
+            ufa.union_set(ai->u, ai->v);
+
     }
-
-    Triangulation tri;
-    tri.insert(j.begin(), j.end());
-
-    Graph G = build_graph(tri, p);
-
-    // Compute the connected components
-    vector<int> component(n);
-    connected_components(G, &component[0]);
-
-    // Check all the missions
-    int missions_completed = 0;
-    for (int i = 0; i < m; i++) {
-        bool cm = check_mission(tri, j, component, s[i], t[i], p);
-        if ( cm ) cout << "y";
-        else cout << "n";
-        if (cm) missions_completed |= 1<<i;
-    }
-    cout << endl;
-
-
-    cout << 4*p << endl;
-    cout << p << endl;
-
-
-
+    if (ai != edges.begin() && (ai-1)->sql > a) a = (ai-1)->sql;
+    if (bi != edges.begin() && (bi-1)->sql > b) b = (bi-1)->sql;
+    cout << endl << a << endl << b << endl;
 }
-
 
 int main() {
     ios_base::sync_with_stdio(false);
+    cout << setiosflags(ios::fixed) << setprecision(0);
     int T; cin >> T;
     for (int t = 0; t < T; t++) {
         goldeneye();

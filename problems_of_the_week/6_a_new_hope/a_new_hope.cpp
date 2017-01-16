94fa4b326ab5d60d7f7f73b97890c088421889c0
Alberto Montes <alm59321@gmail.com>
2017-01-16 10:26:59 +0100

    Solve A New Hope exercise

--- problems_of_the_week/6_a_new_hope/a_new_hope.cpp
+++ problems_of_the_week/6_a_new_hope/a_new_hope.cpp
@@ -1,68 +1,87 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
-// BGL includes
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/push_relabel_max_flow.hpp>
-// Namespaces
+
 using namespace std;
-using namespace boost;
 
-
-// BGL Graph definitions
-// =====================
-// Graph Type with nested interior edge properties for Flow Algorithms
-typedef	adjacency_list_traits<vecS, vecS, directedS> Traits;
-typedef adjacency_list<vecS, vecS, directedS, no_property,
-	property<edge_capacity_t, long,
-		property<edge_residual_capacity_t, long,
-			property<edge_reverse_t, Traits::edge_descriptor> > > >	Graph;
-// Interior Property Maps
-typedef	property_map<Graph, edge_capacity_t>::type		EdgeCapacityMap;
-typedef	property_map<Graph, edge_residual_capacity_t>::type	ResidualCapacityMap;
-typedef	property_map<Graph, edge_reverse_t>::type		ReverseEdgeMap;
-typedef	graph_traits<Graph>::vertex_descriptor			Vertex;
-typedef	graph_traits<Graph>::edge_descriptor			Edge;
-
-
-// Custom Edge Adder Class, that holds the references
-// to the graph, capacity map and reverse edge map
-// ===================================================
-class EdgeAdder {
-	Graph &G;
-	EdgeCapacityMap	&capacitymap;
-	ReverseEdgeMap	&revedgemap;
-
-public:
-	// to initialize the Object
-	EdgeAdder(Graph & G, EdgeCapacityMap &capacitymap, ReverseEdgeMap &revedgemap):
-		G(G), capacitymap(capacitymap), revedgemap(revedgemap){}
-
-	// to use the Function (add an edge)
-	void addEdge(int from, int to, long capacity) {
-		Edge e, reverseE;
-		bool success;
-		tie(e, success) = add_edge(from, to, G);
-		tie(reverseE, success) = add_edge(to, from, G);
-		capacitymap[e] = capacity;
-		capacitymap[reverseE] = 0;
-		revedgemap[e] = reverseE;
-		revedgemap[reverseE] = e;
+struct center {
+	center (int n) {
+		sup = vector<uint16_t>(n, 0);
 	}
+	vector<uint16_t> sup;
+	map<uint16_t, map<int, int> > extsup;
+	map<int, int> bestmap;
 };
 
 
+int best_stormtroopers(vector<center>& centers, int c, int mask, const int S) {
+	auto bestit = centers[c].bestmap.find(mask);
+	if (bestit != centers[c].bestmap.end())
+		return bestit->second;
+
+	int best = 0;
+	for (int k = 0; k < (0x1<<S); k++) {
+		// Mask refers to the stormtroopers that can not be choosen because are being supervised
+		if ((k & mask) != 0) continue;
+
+		bool valid = true;
+		// Check the validity looking if any of the stormtroopers supervise each other in this
+		// configurations
+		for (int i = 0; i < S && valid; i++) {
+			if (((k & (0x1<<i)) != 0) && ((k & centers[c].sup[i]) != 0)) valid = false;
+		}
+		if (!valid) continue;
+
+		int count = 0;
+		// Count the strompers being sabotaged
+		for (int i = 0; i < S; i++) {
+			if (k & (0x1<<i)) count++;
+		}
+		// Now compute the mask (the stormtroopers that on the children centers are being
+		// supervised by this configuration) and with DP count the maximum number of stormtroopers
+		// that are not being supervised by each other.
+		for (auto& esup : centers[c].extsup) {
+			int nmask = 0;
+			for (auto& p : esup.second) {
+				if (k & 0x1<<p.first) nmask |= p.second;
+			}
+			count += best_stormtroopers(centers, esup.first, nmask, S);
+		}
+		best = max(best, count);
+	}
+	centers[c].bestmap[mask] = best;
+	return best;
+}
+
 void a_new_hope() {
-	int n, m, s;
-    cin >> n >> m >> s;
+	int K, S, M;
+    cin >> K >> S >> M;
 
-    Graph G;
-	EdgeCapacityMap capacitymap = get(edge_capacity, G);
-	ReverseEdgeMap revedgemap = get(edge_reverse, G);
-	EdgeAdder eaG(G, capacitymap, revedgemap);
+	vector<center> centers(K, center(S));
 
+	int u, v, h, x, y;
+	for (int i = 0; i < M; i++) {
+		cin >> u >> v >> h;
+		if (u == v) {
+			for (int j = 0; j < h; j++) {
+				cin >> x >> y;
+				centers[u].sup[x] |= 0x1 << y;
+			}
+		}
+		else {
+			map<int, int> esup;
+			for (int j = 0; j < h; j++) {
+				cin >> x >> y;
+				esup[x] |= 0x1 << y;
+			}
+			centers[u].extsup[v] = esup;
+		}
 
+	}
+
+	cout << best_stormtroopers(centers, 0, 0, S) << endl;
 }
 
 int main() {

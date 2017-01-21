5bccfe9674207db432ca0289e5a7ebde85278b80
Alberto Montes <alm59321@gmail.com>
2017-01-21 18:10:27 +0100

    Solve Radiation exercise

--- exercises/radiation/radiation.cpp
+++ exercises/radiation/radiation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cassert>
 #include <cmath>
 #include <CGAL/basic.h>
@@ -16,40 +17,27 @@ typedef CGAL::Quadratic_program_solution<ET> Solution;
 using namespace std;
 
 
-bool solve(vector<vector<long> >& health, vector<vector<long> >& tumor, const int D) {
-    int h = health.size(), t = health.size();
+bool solve(vector<vector<vector<long> > >& coefficients, const int h, const int t, const int d) {
+
+    int nb_coeff = coefficients[0][d].size();
 
     Program lp (CGAL::SMALLER, false, 0, false, 0);
     CGAL::Quadratic_program_options options;
     options.set_pricing_strategy(CGAL::QP_BLAND);     // Bland's rule
 
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int d = 1; d <= D; d++) {
-                lp.set_a(i, j*3+d-1, health[i][j]^d);
-            }
+    for (int i = 0; i < (h + t); i++) {
+        for (int k = 0; k < nb_coeff; k++) {
+            lp.set_a(k, i, coefficients[i][d][k]);
         }
-        lp.set_a(i, D*3, 1);    // Epsilon
-
         lp.set_b(i, 0);
-        lp.set_u(i, CGAL::LARGER);
-    }
 
-    for (int i = 0; i < t; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int d = 1; d <= D; d++) {
-                lp.set_a(i, j*3+d, tumor[i][j]^d);
-            }
-        }
-        lp.set_a(i, D*3, 1);    // Epsilon
-
-        lp.set_b(i, 0);
-        lp.set_u(i, CGAL::SMALLER);
+        if (i >= h) lp.set_a(nb_coeff, i, 1); // Epsilon
     }
-    lp.set_c(D*3, 1);
+    lp.set_c(nb_coeff, -1);
+    lp.set_l(nb_coeff, true, 0);
 
     Solution s = CGAL::solve_linear_program(lp, ET(), options);
-    return !s.is_infeasible();
+    return s.is_unbounded();
 }
 
 
@@ -57,13 +45,51 @@ void radiation(){
     int h, t;
     cin >> h >> t;
 
+    const int D = 30;
+
     vector<vector<long> > health(h, vector<long>(3)), tumor(t,vector<long>(3));
 
-    for (int i = 0; i < h; i++) cin >> health[i][0] >> health[i][1] >> health[i][3];
-    for (int i = 0; i < t; i++) cin >> tumor[i][0] >> tumor[i][1] >> tumor[i][3];
+    for (int i = 0; i < h; i++) cin >> health[i][0] >> health[i][1] >> health[i][2];
+    for (int i = 0; i < t; i++) cin >> tumor[i][0] >> tumor[i][1] >> tumor[i][2];
 
+    vector<vector<vector<long> > >coefficients(h+t, vector<vector<long> >(D+1));
 
+    for (int i = 0; i < (h + t); i++) {
+        vector<long> cell = (i < h) ? health[i]: tumor[i-h];
 
+        for (int d = 1; d <= D; d++) {
+            long coef_x = 1;
+            for (int x = 0; x <= d; x++) {
+                long coef_y = 1;
+                for (int y = 0; y <= d; y++) {
+                    long coef_z = 1;
+                    for (int z = 0; z <= d; z++) {
+                        if (x + y + z > d) continue;
+                        if (i >=h) coefficients[i][d].push_back(coef_x * coef_y * coef_z);
+                        else coefficients[i][d].push_back(-coef_x * coef_y * coef_z);
+                        coef_z *= cell[2];
+                    }
+                    coef_y *= cell[1];
+                }
+                coef_x *= cell[0];
+            }
+        }
+    }
+
+    int upper = 1;
+    while (!solve(coefficients, h, t, upper) && upper <= D) upper *= 2;
+
+    if (upper > D) cout << "impossible" << endl;
+
+    int low = upper / 2;
+    while (low + 1 != upper) {
+        int mid = low + (upper - low) / 2;
+        if (solve(coefficients, h, t, mid)) {
+            upper = mid;
+        } else low = mid;
+    }
+
+    cout << upper << endl;
 }
 
 

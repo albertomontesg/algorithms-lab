42478e22dd9e456b334e8ea552984c6fd9a1c34e
Alberto Montes <alm59321@gmail.com>
2017-01-22 22:30:58 +0100

    Working on Punch

--- exercises/punch/punch.cpp
+++ exercises/punch/punch.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <cassert>
-#include <CGAL/basic.h>
-#include <CGAL/QP_models.h>
-#include <CGAL/QP_functions.h>
-#include <CGAL/Gmpz.h>
-
-// choose exact integral type
-typedef CGAL::Gmpz ET;
-
-// program and solution types
-typedef CGAL::Quadratic_program<int> Program;
-typedef CGAL::Quadratic_program_solution<ET> Solution;
 
 using namespace std;
 
 void punch(){
     int n, k; cin >> n >> k;
 
-    Program qp (CGAL::EQUAL, true, 0, false, 0);
+    vector<pair<int, int> > baverages(n);
     int c, v;
     for (int i = 0; i < n; i++) {
         cin >> c >> v;
-        qp.set_c(i, c);
-        qp.set_a(i, 0, v);
-        // qp.set_d(i, i, 1);
+        baverages[i] = make_pair(c, v);
     }
-    qp.set_b(0, k);
 
-    Solution s = CGAL::solve_linear_program(qp, ET());
-    assert(s.solves_linear_program(qp));
-    assert(s.is_optimal());
+    vector<vector<pair<int, int> > > T(n, vector<pair<int, int> >(k, pair<int, int>(-1, -1)));
 
-    // CGAL::Quadratic_program_solution<ET>::Variable_value_iterator opt = s.variable_values_begin();
-    // int value = 0;
-    // for (; opt != s.variable_values_end(); opt++) {
-    //     int tmp = int(CGAL::to_double(*(opt)));
-    //     if (tmp > 0) value++;
-    // }
-    // cout << value << endl;
-    cout << CGAL::to_double(s.objective_value()) << endl;
+    for (int i = 0; i < n; i++) {
+        c = baverages[i].first;
+        v = baverages[i].second;
+        for (int j = 0; j < k; j++) {
+            if (i == 0) {
+                if ((j + 1) % v != 0) continue;
+                int nb = (j + 1) / v;
+                T[i][j] = make_pair(nb*c, 1);
+            }
+            
+        }
+    }
 
 }
 

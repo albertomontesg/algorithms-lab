6c4730a0eb9a4015f5fcd19489ec62fcae5b7165
Alberto Montes <alm59321@gmail.com>
2017-01-29 20:53:14 +0100

    Solve Defensive Line exercise (from exam)

--- exercises/defensive_line/defensive_line.cpp
+++ exercises/defensive_line/defensive_line.cpp
@@ -16,7 +16,7 @@ void defensive_line() {
   while (e < N) {
     while (e < N && sum < K) sum += values[++e];
 
-    if (sum == K) intervals.push_back(make_pair(b, e));
+    if (sum == K) intervals.push_back(make_pair(e, b));
 
     do {
       sum -= values[b++];
@@ -28,11 +28,33 @@ void defensive_line() {
     return;
   }
 
-  // Now DP to find the set of N intervals that do not overlap each other
+  sort(intervals.begin(), intervals.end());
 
-  // for (int i = 0; i < intervals.size(); i++) {
-  //   cout << intervals[i].first << " " << intervals[i].second << endl;
-  // }
+  // Now DP to find the set of N intervals that do not overlap each other
+  vector<vector<int> > T(M+1, vector<int>(N, -1));
+  for (int i = 0; i < N; i++) T[0][i] = 0;
+
+  for (int n = 0; n < N; n++) {
+    for (int m = 1; m <= M; m++) {
+      pair<int,int> interval = *lower_bound(intervals.begin(), intervals.end(), make_pair(n,-1));
+
+      // In case there is no interval ending at position n
+      if (interval.first != n) {
+        // In case being at the first interval
+        if (n == 0) T[m][n] = T[m-1][n];
+        else T[m][n] = max(T[m-1][n], T[m][n-1]);
+        continue;
+      }
+
+      // There is a interval that ends at n
+      int num = interval.first - interval.second + 1;
+
+      if (m == 1 || interval.second-1 < 0) T[m][n] = max(num, T[m][n-1]);
+      else T[m][n] = max(num+T[m-1][interval.second-1], T[m][n-1]);
+    }
+  }
+
+  cout << T[M][N-1] << endl;
 }
 
 int main() {
